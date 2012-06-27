90e34d3aec691c8f44ccfa9d2a01f18364b1ec60
Angelo Dini <finalangeljp@hotmail.com>
2012-06-27 17:59:32 +0200

    added djeese example

---
+++ static/js/base.js
@@ -0,0 +1,11 @@
+/*!
+ * @author:		Angelo Dini
+ * @copyright:	http://www.divio.ch under the BSD Licence
+ * @requires:	jQuery
+ */
+
+//##################################################
+// #BASE.JS#
+(function($){
+
+})(jQuery);
